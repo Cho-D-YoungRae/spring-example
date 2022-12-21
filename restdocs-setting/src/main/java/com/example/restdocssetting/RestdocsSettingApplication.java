32b23428daeaadd12d536e2346e59930d0b864d0
Cho-D-YoungRae <yrc9229@gmail.com>
2022-12-21 22:54:32 +0900

    spring-rest-docs setting

---
+++ restdocs-setting/src/main/java/com/example/restdocssetting/RestdocsSettingApplication.java
@@ -0,0 +1,13 @@
+package com.example.restdocssetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdocsSettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestdocsSettingApplication.class, args);
+    }
+
+}
---
+++ restdocs-setting/src/test/java/com/example/restdocssetting/RestdocsSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restdocssetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdocsSettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
